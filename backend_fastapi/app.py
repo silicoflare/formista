4131d769d16d573da9a66efcedaf7d647a40bbbd
Suraj B M <silicoflare@gmail.com>
2023-11-07 10:57:40 +0530

    fixed CORS error

--- backend_fastapi/app.py
+++ backend_fastapi/app.py
@@ -14,7 +14,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost", "http://localhost:3000", "*"],
+    allow_origins=["http://localhost", "http://localhost:3000", "https://formista.vercel.app" "*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
